5bed3c70efe3202b2c0e1080c197c9a362b12e56
최민석 <min@choemineoguiMBP.lan>
2016-10-09 00:09:22 +0900

    * chapther 5.1

--- python/5. Data Structures/5.1. More on Lists.py
+++ python/5. Data Structures/5.1. More on Lists.py
@@ -40,12 +40,12 @@ print(stack)
 print(stack.pop())
 print(stack)
 
-
 # 5.1.2 Using Lists as Queues
 '''
 collections.deque를 사용
 '''
 from collections import deque
+
 queue = deque(["Eric", "John", "Michael"])
 queue.append("Terry")
 queue.append("Graham")
@@ -60,10 +60,64 @@ print(type(queue), queue)
 
 squares = []
 for x in range(10):
-    squares.append(x**2)
+    squares.append(x ** 2)
 
 print(squares)
 
-squares = list(map(lambda x: x**2, range(10)))
+squares = list(map(lambda x: x ** 2, range(10)))
 
 print(squares)
+
+squares = [x ** 2 for x in range(10)]
+
+print(squares)
+
+print([(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y])
+combs = []
+for x in [1, 2, 3]:
+    for y in [3, 1, 4]:
+        if x != y:
+            combs.append((x, y))
+print(combs)
+
+vec = [-4, -2, 0, 2, 4]
+print([x * 2 for x in vec])
+print([x for x in vec if x >= 0])
+print([abs(x) for x in vec])
+
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+print([weapon.strip() for weapon in freshfruit])
+
+print([(x, x ** 2) for x in range(6)])
+
+vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print([num for elem in vec for num in elem])
+t = []
+for elem in vec:
+    for num in elem:
+        t.append(num)
+print(t)
+
+from math import pi
+
+print([str(round(pi, i)) for i in range(1, 6)])
+
+# 5.1.4. Nested List Comprehensions
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+print([[row[i] for row in matrix] for i in range(4)])
+m = []
+for i in range(4):
+    temp = []
+    for row in matrix:
+        temp.append(row[i])
+    m.append(temp)
+
+print(m)
+
+print(list(zip(*matrix)))
